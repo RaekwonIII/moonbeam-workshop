d9eae82288e24e312e7f0a182bed77f061665ead
Massimo Luraschi <massimo.lura@gmail.com>
2022-04-29 10:11:25 +0200

    Testing workshop

--- src/model/generated/index.ts
+++ src/model/generated/index.ts
@@ -1,2 +1,4 @@
-export * from "./account.model"
-export * from "./historicalBalance.model"
+export * from "./token.model"
+export * from "./owner.model"
+export * from "./contract.model"
+export * from "./transfer.model"
--- src/processor.ts
+++ src/processor.ts
@@ -1,96 +1,65 @@
-import * as ss58 from "@subsquid/ss58";
-import {
-  EventHandlerContext,
-  Store,
-  SubstrateProcessor,
-} from "@subsquid/substrate-processor";
-import { lookupArchive } from "@subsquid/archive-registry";
-import { Account, HistoricalBalance } from "./model";
-import { BalancesTransferEvent } from "./types/events";
-
-const processor = new SubstrateProcessor("kusama_balances");
-
-processor.setBatchSize(500);
+import {assertNotNull, EvmLogHandlerContext, SubstrateEvmProcessor} from "@subsquid/substrate-evm-processor"
+import {contract, createContractEntity, getContractEntity} from "./contract"
+import * as erc721 from "./abi/erc721"
+import {Owner, Token, Transfer} from "./model"
+ 
+const processor = new SubstrateEvmProcessor('erc721')
+ 
 processor.setDataSource({
-  archive: lookupArchive("kusama")[0].url,
-  chain: "wss://kusama-rpc.polkadot.io",
+    chain: "wss://wss.api.moonriver.moonbeam.network",
+    archive: assertNotNull(process.env.ARCHIVE)
 });
-
-processor.addEventHandler("balances.Transfer", async (ctx) => {
-  const transfer = getTransferEvent(ctx);
-  const tip = ctx.extrinsic?.tip || 0n;
-  const from = ss58.codec("kusama").encode(transfer.from);
-  const to = ss58.codec("kusama").encode(transfer.to);
-
-  const fromAcc = await getOrCreate(ctx.store, Account, from);
-  fromAcc.balance = fromAcc.balance || 0n;
-  fromAcc.balance -= transfer.amount;
-  fromAcc.balance -= tip;
-  await ctx.store.save(fromAcc);
-
-  const toAcc = await getOrCreate(ctx.store, Account, to);
-  toAcc.balance = toAcc.balance || 0n;
-  toAcc.balance += transfer.amount;
-  await ctx.store.save(toAcc);
-
-  await ctx.store.save(
-    new HistoricalBalance({
-      id: `${ctx.event.id}-to`,
-      account: fromAcc,
-      balance: fromAcc.balance,
-      date: new Date(ctx.block.timestamp),
-    })
-  );
-
-  await ctx.store.save(
-    new HistoricalBalance({
-      id: `${ctx.event.id}-from`,
-      account: toAcc,
-      balance: toAcc.balance,
-      date: new Date(ctx.block.timestamp),
-    })
-  );
+ 
+processor.addPreHook({range: {from: 0, to: 0}}, async ctx => {
+    await ctx.store.save(createContractEntity())
 });
-
-processor.run();
-
-interface TransferEvent {
-  from: Uint8Array;
-  to: Uint8Array;
-  amount: bigint;
-}
-
-function getTransferEvent(ctx: EventHandlerContext): TransferEvent {
-  const event = new BalancesTransferEvent(ctx);
-  if (event.isV1020) {
-    const [from, to, amount] = event.asV1020;
-    return { from, to, amount };
-  } else if (event.isV1050) {
-    const [from, to, amount] = event.asV1050;
-    return { from, to, amount };
+ 
+processor.addEvmLogHandler(
+  contract.address,
+  {
+      filter: [
+          erc721.events['Transfer(address,address,uint256)'].topic
+      ]
+  },
+  processTransfer
+);
+ 
+async function processTransfer(ctx: EvmLogHandlerContext): Promise<void> {
+  const transfer = erc721.events['Transfer(address,address,uint256)'].decode(ctx)
+ 
+  let from = await ctx.store.get(Owner, transfer.from)
+  if (from == null) {
+      from = new Owner({id: transfer.from, balance: 0n})
+      await ctx.store.save(from)
+  }
+ 
+  let to = await ctx.store.get(Owner, transfer.to)
+  if (to == null) {
+      to = new Owner({id: transfer.to, balance: 0n})
+      await ctx.store.save(to)
+  }
+ 
+  let token = await ctx.store.get(Token, transfer.tokenId.toString())
+  if (token == null) {
+      token = new Token({
+          id: transfer.tokenId.toString(),
+          uri: await contract.tokenURI(transfer.tokenId),
+          contract: await getContractEntity(ctx),
+          owner: to
+      })
+      await ctx.store.save(token)
   } else {
-    const { from, to, amount } = event.asV9130;
-    return { from, to, amount };
+      token.owner = to
+      await ctx.store.save(token)
   }
-}
-
-async function getOrCreate<T extends { id: string }>(
-  store: Store,
-  EntityConstructor: EntityConstructor<T>,
-  id: string
-): Promise<T> {
-  let entity = await store.get<T>(EntityConstructor, {
-    where: { id },
-  });
-
-  if (entity == null) {
-    entity = new EntityConstructor();
-    entity.id = id;
-  }
-
-  return entity;
-}
-
-type EntityConstructor<T> = {
-  new (...args: any[]): T;
+ 
+  await ctx.store.save(new Transfer({
+      id: ctx.txHash,
+      token,
+      from,
+      to,
+      timestamp: BigInt(ctx.substrate.block.timestamp),
+      block: ctx.substrate.block.height,
+      transactionHash: ctx.txHash
+  }))
 };
